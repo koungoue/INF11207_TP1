a73d32de1c298e1094530f3ea88c3aaef0a1c628
channel <koungouechannel@gmail.com>
2026-02-21 10:44:43 -0500

    algorithme de tri implementé

---
+++ INF11207_TP1/ITrie.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace INF11207_TP1
+{
+    internal interface ITrie
+    {
+        void Trier(List<Voisins> voisins);
+    }
+}
